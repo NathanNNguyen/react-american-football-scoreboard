92ec49558ba0e509ddd8978a6d88d1e5ae601ce9
Nhan <natenguyen51294@gmail.com>
2020-04-13 09:55:38 -0700

    added timer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [lionsScore, setLionsScore] = useState(0);
   const [tigersScore, setTigersAwayScore] = useState(0);
-  const [quarter, setQuarter] = useState(0);
+  const [quarter, setQuarter] = useState(1);
 
   const quarterNext = () => {
     if (quarter < 4) {
